b906ee8c91aba29cff505e9bf397e697abf48d03
davidabutler92 <davidabutler92@yahoo.com>
2020-12-09 15:53:34 -0800

    add 3 endpoints with passing tests

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,6 +2,7 @@ const fs = require('fs');
 const request = require('supertest');
 const app = require('../lib/app');
 const pool = require('../lib/utils/pool');
+const Book = require('../lib/models/Book');
 
 describe('app endpoints', () => {
 
@@ -15,12 +16,65 @@ describe('app endpoints', () => {
 
   it('should create a new book using POST', async() => {
     const res = await request(app)
-      .post('/ap1/v1/books')
+      .post('/api/v1/books')
       .send({
         title: 'Harry Potter',
         author: 'J.K. Rowling',
         genre: 'Fantasy'
       });
+      
+    expect(res.body).toEqual({
+      id: '1',
+      title: 'Harry Potter',
+      author: 'J.K. Rowling',
+      genre: 'Fantasy'
+    });
   });
+
+  it('should get a book by id using GET', async() => {
+    const book = await Book.insert({ 
+      title: 'Eragon', 
+      author: 'Christopher Paolini', 
+      genre: 'Fantasy' 
+    });
+
+    const res = await request(app)
+      .get(`/api/v1/books/${book.id}`);
+
+    expect(res.body).toEqual(book);
+  });
+
+  it('should update a book by id using PUT', async() => {
+    const book = await Book.insert({
+      title: 'A new book',
+      author: 'David Butler',
+      genre: 'Horror'
+    });
+
+    const res = await request(app)
+      .put(`/api/v1/books/${book.id}`)
+      .send({
+        title: 'A new book',
+        author: 'David Butler',
+        genre: 'Horror'
+      });
+
+    expect(res.body).toEqual({
+      id: book.id,
+      title: 'A new book',
+      author: 'David Butler',
+      genre: 'Horror'
+    });
+  });
+
+  it('should delete a book using DELETE', async() => {
+    const book = await Book.insert({ title: 'Harry Potter', author: 'J.K. Rowling', genre: 'Fantasy' });
+
+    const res = await request(app)
+      .delete(`/api/v1/books/${book.id}`);
+
+    expect(res.body).toEqual(book);
+  });
+
 
 });
